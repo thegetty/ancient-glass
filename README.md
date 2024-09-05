1b794106ea468904aab05f550a385abe5cf74eea
Greg Albers <galbers@getty.edu>
2024-09-05 09:16:09 -0700

    Add thumb shortcode

--- README.md
+++ README.md
@@ -95,6 +95,10 @@ Add custom def shortcode for glossary pop-ups
 **_plugins/shortcodes/index.js**
 Add custom def shortcode for accession number catalogue links
 
+**_plugins/shortcodes/thumb.js**
+**_plugins/shortcodes/index.js**
+Add custom thumb shortcode to create thumbnails based on cat numbers
+
 **content/_assets/javascript/application/index.js**
 Display only one pop-up at a time
 
---
+++ _plugins/shortcodes/thumb.js
@@ -0,0 +1,51 @@
+//
+// CUSTOMIZED FILE
+// New shortcode to take cat number and return a thumbnail image
+// with the cat and accession numbers, and linked to the main entry page
+//
+const chalkFactory = require('~lib/chalk')
+const { renderOneLine } = require('~lib/common-tags')
+const { warn } = chalkFactory('shortcodes:cat')
+ 
+/**
+* Shortcode to create thumbnail links from cat numbers
+*
+* @param      {String}   catNumber    The object cat number
+* @return     {boolean}  An HTML <figure> element
+*/
+module.exports = function (eleventyConfig, { page }) {
+  const objects = eleventyConfig.globalData.objects
+    ? eleventyConfig.globalData.objects.object_list
+    : []
+  const figures = eleventyConfig.globalData.figures
+    ? eleventyConfig.globalData.figures.figure_list
+    : []
+ 
+  return function (catNumber) {
+    const catPrefix = 'cat-'
+    const catId = catPrefix.concat(catNumber)
+    const obj = objects.find((object) => object.id === catId)
+ 
+    if (!obj) {
+      warn(`Object entry not found for '${catNumber}'`)
+      return renderOneLine`<span style="background-color:yellow;">${catNumber}</span>`
+    }
+    
+    const catPath = '/catalogue/'
+    const catLink = catPath.concat(catId,'/')
+    const catAccNo = obj.accession_number
+    const catFigureId = obj.figure[0].id
+
+    const fig = figures.find((figure) => figure.id === catFigureId)
+    const figPath = fig.src.replace('figures', catFigureId).replace('.jpg','')
+
+    return renderOneLine`
+      <figure class="cat-thumbnail">
+        <a href="${catLink}">
+        <img src="/iiif/${figPath}/thumbnail.jpg" />
+        <figcaption><strong>${catNumber}</strong>&nbsp;(${catAccNo})</figcaption>
+        </a>
+      </figure>
+      `
+  }
+}
